c9e05df3f145ed7c3899b362e0feb71126c098bd
hypertheosx <hypertheosx@gmail.com>
2026-06-09 22:12:20 +0500

    add LoginPage

---
+++ src/test/java/ru/hypertheosx/qa/pages/LoginPage.java
@@ -0,0 +1,37 @@
+package ru.hypertheosx.qa.pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static ru.hypertheosx.qa.components.ButtonLabels.LOGIN_BUTTON;
+import static ru.hypertheosx.qa.components.ElementFactory.inputField;
+import static ru.hypertheosx.qa.components.InputLabels.PASSWORD;
+import static ru.hypertheosx.qa.components.InputLabels.USERNAME;
+
+public class LoginPage {
+
+    public SelenideElement usernameInput = inputField(USERNAME);
+    public SelenideElement passwordInput = inputField(PASSWORD);
+    public SelenideElement loginBtn = inputField(LOGIN_BUTTON);
+
+    private final SelenideElement loginError = $("[data-test='error']");
+
+    public LoginPage withUsername(String username) {
+        usernameInput.setValue(username);
+        return this;
+    }
+
+    public LoginPage withPassword(String password) {
+        passwordInput.setValue(password);
+        return this;
+    }
+
+    public void submitLogin() {
+        loginBtn.click();
+    }
+
+    public String getErrorMessage() {
+        return loginError.getText();
+    }
+
+}
