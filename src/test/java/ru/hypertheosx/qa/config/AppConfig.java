c6bfe5fcedd906a341b5599c522717f1da17faa7
hypertheosx <hypertheosx@gmail.com>
2026-06-09 22:12:10 +0500

    add appconfig

---
+++ src/test/java/ru/hypertheosx/qa/config/AppConfig.java
@@ -0,0 +1,8 @@
+package ru.hypertheosx.qa.config;
+
+public class AppConfig {
+
+    public final static String BASE_URL = "https://www.saucedemo.com";
+
+    public static final String PASSWORD = "secret_sauce";
+}
